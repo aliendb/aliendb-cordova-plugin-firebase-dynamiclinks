1392f9a1cd6d342c8b2af6c8ca021cff87728192
D <12778164+aliendb@users.noreply.github.com>
2018-08-31 21:51:39 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# aliendb-cordova-plugin-firebase-dynamiclinks
+# cordova-plugin-firebase-dynamiclinks<br>[![NPM version][npm-version]][npm-url] [![NPM downloads][npm-downloads]][npm-url]
+> Cordova plugin for [Firebase Dynamic Links](https://firebase.google.com/docs/dynamic-links/)
+ 
+## Installation
+
+    cordova plugin add cordova-plugin-firebase-dynamiclinks --save --variable APP_DOMAIN="example.com" --variable PAGE_LINK_DOMAIN="example.page.link"
+
+Use variable `APP_DOMAIN` specify web URL where your app will start an activity to handle the link.
+
+Use variable `PAGE_LINK_DOMAIN` specify your `*.page.link` domain.
+
+Use variable `FIREBASE_DYNAMIC_LINKS_VERSION` to override dependency version on Android.
+
+## Supported Platforms
+
+- iOS
+- Android
+
+
+## Methods
+
+### onDynamicLink(_callback_)
+Registers callback that is triggered on each dynamic link click.
+```js
+cordova.plugins.firebase.dynamiclinks.onDynamicLink(function(data) {
+    console.log("Dynamic link click with data: ", data);
+});
+```
+
+Software taken from chemerisuk/cordova-plugin-firebase-dynamiclinks
---
+++ src/android/FirebaseDynamicLinksPlugin.java
@@ -0,0 +1,62 @@
+package aliendb.cordova.firebase;
+
+import android.content.Intent;
+
+import aliendb.cordova.firebase.CordovaMethod;
+import aliendb.cordova.firebase.ReflectiveCordovaPlugin;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class FirebaseDynamicLinksPlugin extends ReflectiveCordovaPlugin {
+    private static final String TAG = "FirebaseDynamicLinks";
+
+    private CallbackContext dynamicLinkCallback;
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        if (this.dynamicLinkCallback != null) {
+            respondWithDynamicLink(intent);
+        }
+    }
+
+    @CordovaMethod
+    private void onDynamicLink(CallbackContext callbackContext) {
+        this.dynamicLinkCallback = callbackContext;
+
+        respondWithDynamicLink(cordova.getActivity().getIntent());
+    }
+
+    private void respondWithDynamicLink(Intent intent) {
+        FirebaseDynamicLinks.getInstance().getDynamicLink(intent)
+            .continueWith(new Continuation<PendingDynamicLinkData, JSONObject>() {
+                @Override
+                public JSONObject then(Task<PendingDynamicLinkData> task) throws JSONException {
+                    PendingDynamicLinkData data = task.getResult();
+
+                    JSONObject result = new JSONObject();
+                    result.put("deepLink", data.getLink());
+                    result.put("clickTimestamp", data.getClickTimestamp());
+                    result.put("minimumAppVersion", data.getMinimumAppVersion());
+
+                    if (dynamicLinkCallback != null) {
+                        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, result);
+                        pluginResult.setKeepCallback(true);
+                        dynamicLinkCallback.sendPluginResult(pluginResult);
+                    }
+
+                    return result;
+                }
+            });
+    }
+}
