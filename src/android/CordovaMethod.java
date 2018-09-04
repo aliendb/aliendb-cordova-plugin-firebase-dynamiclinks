ef8579156760769aba106a28c0c3c19579222e06
D <12778164+aliendb@users.noreply.github.com>
2018-09-03 21:26:24 -0400

    Update CordovaMethod.java

--- src/android/CordovaMethod.java
+++ src/android/CordovaMethod.java
@@ -1,6 +1,6 @@
 package aliendb.cordova.firebase;
 
-import ReflectiveCordovaPlugin.ExecutionThread;
+import aliendb.cordova.firebase.ReflectiveCordovaPlugin.ExecutionThread;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
